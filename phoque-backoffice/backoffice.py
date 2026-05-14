a4ffd03fdd02a671fc5f67cc56aa5870726f60ca
Timst <timst44@gmail.com>
2026-05-14 00:35:10 -0700

    Add heartbeat to backoffice client

--- phoque-backoffice/backoffice.py
+++ phoque-backoffice/backoffice.py
@@ -1,15 +1,23 @@
+import asyncio
+
 import requests
 from gpiozero import Button
 from signal import pause
 
-SERVER_URL = "http://192.168.1.1:8000/serve"
+SERVER_URL = "http://192.168.1.1:8000"
 GPIO_PIN = 18
 
 call_button = Button(GPIO_PIN, pull_up=True, bounce_time=0.1)
 
+async def emit_heartbeat():
+    while True:
+        requests.get(SERVER_URL + "/heartbeat/", timeout=3)
+
+        await asyncio.sleep(5)
+
 def call_next_number():
     try:
-        response = requests.get(SERVER_URL, timeout=3)
+        response = requests.get(SERVER_URL + "/serve", timeout=3)
 
         if response.status_code == 200:
             print(f"Success: Called ticket {response.text}")
@@ -20,5 +28,7 @@ def call_next_number():
         print(f"Network error: {e}")
 
 call_button.when_pressed = call_next_number
+
+emit_heartbeat()
 
 pause()
